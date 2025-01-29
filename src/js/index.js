d6336d23e9c61db068eedf41031bd9b94cc93015
Andresampsouza <andresa.souzamp@gmail.com>
2025-01-29 10:52:32 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,19 @@
-
 const btnAvancar = document.getElementById("btn-avancar");
 const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
+
+cartoes.forEach(cartao => {
+    cartao.addEventListener("click", function () {
+        const cartaVirada = cartao.querySelector(".carta-virada");
+
+        cartao.classList.toggle("virar");
+        cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+        const descricao = cartao.querySelector(".descricao")
+        descricao.classList.toggle("esconder");
+    });
+});
 
 btnAvancar.addEventListener("click", function () {
     if (cartaoAtual === cartoes.length - 1) { return; }
@@ -13,7 +24,6 @@ btnAvancar.addEventListener("click", function () {
     mostrarCartao(cartaoAtual);
 
 });
-
 
 btnVoltar.addEventListener("click", function () {
     if (cartaoAtual === 0) return;
